a3dab7e8b7be8c5f84427aed67ef01df3a18b930
alamin111 <alaminshakil111@gmail.com>
2024-10-24 11:53:51 +0600

    misitUPUpdate

--- HTML/HTML_ComponentSelfWork/MixitUp/assets/js/main.js
+++ HTML/HTML_ComponentSelfWork/MixitUp/assets/js/main.js
@@ -1,11 +1,38 @@
 
 const tabs = document.querySelectorAll('.tab');
+const allTab = document.querySelector('.all');
+const allTabSlider = document.querySelector('.allTabeSlider');
 const mixitUpBoxSliders = document.querySelectorAll('.mixitUpBoxSlider');
 
+// document.addEventListener('DOMContentLoaded', function(){
+//     mixitUpBoxSliders.forEach((slide, index) =>{
+//         if(index != 0){
+//             slide.style.display= 'none';
+//         }
+//     });
+// });
 document.addEventListener('DOMContentLoaded', function(){
-    mixitUpBoxSliders.forEach((slide, index) =>{
-        if(index != 0){
-            slide.style.display= 'none';
-        }
+    document.querySelector('.allTabSlider').textContent = mixitUpBoxSliders;
+});
+
+function allTabSliderShow(){
+    allTab.addEventListener('click',function(){
+        mixitUpBoxSliders.forEach(slider => {
+            // Set display to 'flex' to show all sliders
+            slider.style.display = 'flex';
+        });
+    });
+}
+allTabSliderShow();
+
+tabs.forEach((tab,index)=>{
+    tab.addEventListener('click',function(){
+        mixitUpBoxSliders.forEach((slider)=>{
+            slider.style.display = 'none';
+        });
+        mixitUpBoxSliders[index].style.display = 'flex';
     });
 });
+
+
+
